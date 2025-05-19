99548366eb2a54e17189150d3ff0d2655528b46e
Abd Eldjallil Meskali <155311349+djalilmsk@users.noreply.github.com>
2025-05-19 22:36:00 +0100

    adding vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,47 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["room.svg"],
+      manifest: {
+        name: "room",
+        short_name: "room",
+        description: "room reservation system",
+        theme_color: "#191919",
+        icons: [
+          {
+            src: "room-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "room-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "room-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
