1e8b160b62305f63f76ccb343250ef2a4e45e12b
Abd Eldjallil Meskali <155311349+djalilmsk@users.noreply.github.com>
2025-05-22 23:05:44 +0100

    continue with google loader

--- src/pages/auth/ContinueWithGoogle.jsx
+++ src/pages/auth/ContinueWithGoogle.jsx
@@ -4,6 +4,9 @@ import { customFetch } from "@/utils";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login } from "@/utils/redux/user";
+import toast from "react-hot-toast";
+import { defaults } from "@/utils/format/toast-styles";
+import { Loader } from "lucide-react";
 
 function ContinueWithGoogle() {
   const navigate = useNavigate();
@@ -16,8 +19,20 @@ function ContinueWithGoogle() {
     },
   });
 
-  if (isError) return <>Error</>;
-  if (isLoading) return <>Loading</>;
+  if (isError) {
+    toast.error("Something went wrong", { style: defaults });
+    return navigate("/auth/signup");
+  }
+
+  if (isLoading)
+    return (
+      <div className="absolute top-40 left-1/2 flex w-dvw -translate-x-1/2 items-start justify-center">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <Loader className="size-10 animate-spin" />
+          <div className="text-2xl">Loading</div>
+        </div>
+      </div>
+    );
 
   if (data) {
     dispatch(login({ data: data }));
